89c66672dc4c7a3c0ebf415e93f3c03de8a6ec37
DiegoEstela <die.estela@gmail.com>
2023-06-05 19:26:22 +0200

    cancel product

--- src/controllers/products.controllers.js
+++ src/controllers/products.controllers.js
@@ -2,9 +2,12 @@ const pool = require("../db/index");
 
 const getAllProducts = async (req, res) => {
   try {
-    const query = "SELECT * FROM products ORDER BY concepto ASC";
-    const allProducts = await pool.query(query);
-    res.json(allProducts.rows);
+    const query =
+      "SELECT * FROM products WHERE deleted = $1 ORDER BY concepto ASC";
+    const values = [false];
+    const result = await pool.query(query, values);
+    const prodcuts = result.rows;
+    res.status(200).json(prodcuts);
   } catch (error) {
     console.log(error);
   }
@@ -12,13 +15,14 @@ const getAllProducts = async (req, res) => {
 
 const createProducts = async (req, res, next) => {
   try {
-    const { concepto, precio } = req.body;
+    const { idUsuario, concepto, monto } = req.body;
     const query =
-      "INSERT INTO products (concepto, precio) VALUES ($1, $2) RETURNING *";
-    const values = [concepto, precio];
+      "INSERT INTO products (idUsuario, concepto, monto, deleted) VALUES ($1, $2, $3 , $4)";
+    const values = [idUsuario, concepto, monto, false];
     const result = await pool.query(query, values);
+
     if (result) {
-      res.send("creating a Product");
+      res.status(200).json({ message: "creating a products" });
     }
   } catch (error) {
     console.log(error);
@@ -28,10 +32,10 @@ const createProducts = async (req, res, next) => {
 const updateProducts = async (req, res, next) => {
   try {
     const { id } = req.params;
-    const { concepto, precio } = req.body;
+    const { concepto, monto } = req.body;
     const query =
-      "UPDATE products SET concepto = $1, precio = $2 WHERE id = $3 RETURNING *";
-    const values = [concepto, precio, id];
+      "UPDATE products SET concepto = $1, monto = $2 WHERE idProducto = $3 RETURNING *";
+    const values = [concepto, monto, id];
     const result = await pool.query(query, values);
     if (result.rowCount === 0) {
       res.status(404).send("El producto no existe.");
@@ -43,4 +47,24 @@ const updateProducts = async (req, res, next) => {
   }
 };
 
-module.exports = { getAllProducts, createProducts, updateProducts };
+const CancelProduct = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const query = "UPDATE products SET deleted = $1 WHERE idProducto = $2";
+    const values = [true, id];
+
+    const result = await pool.query(query, values);
+    if (result) {
+      res.status(200).json({ message: "cancel product" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = {
+  getAllProducts,
+  createProducts,
+  updateProducts,
+  CancelProduct,
+};
--- src/routes/produtcs.routes.js
+++ src/routes/produtcs.routes.js
@@ -3,11 +3,13 @@ const {
   getAllProducts,
   createProducts,
   updateProducts,
+  CancelProduct,
 } = require("../controllers/products.controllers");
 const router = Router();
 
 router.get("/products", getAllProducts);
 router.post("/products", createProducts);
 router.put("/products/:id", updateProducts);
+router.put("/cancelproducts/:id", CancelProduct);
 
 module.exports = router;
