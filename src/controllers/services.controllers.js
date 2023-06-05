8c9db7767568c0f8bccd23ba1c030fb7f830a6fc
DiegoEstela <die.estela@gmail.com>
2023-06-05 15:20:58 +0200

    update services

--- src/controllers/services.controllers.js
+++ src/controllers/services.controllers.js
@@ -2,10 +2,12 @@ const pool = require("../db/index");
 
 const getAllServices = async (req, res) => {
   try {
-    const allServices = await pool.query(
-      "SELECT * FROM services ORDER BY concepto ASC"
-    );
-    res.json(allServices.rows);
+    const query =
+      "SELECT * FROM services WHERE deleted = $1 ORDER BY concepto ASC";
+    const values = [false];
+    const result = await pool.query(query, values);
+    const services = result.rows;
+    res.status(200).json({ services });
   } catch (error) {
     console.log(error);
   }
@@ -13,17 +15,33 @@ const getAllServices = async (req, res) => {
 
 const createServices = async (req, res, next) => {
   try {
-    const { concepto } = req.body;
-    const result = await pool.query(
-      "INSERT INTO services (concepto) VALUES ($1)",
-      [concepto]
-    );
+    const { idUsuario, concepto } = req.body;
+    const query =
+      "INSERT INTO services (idUsuario, concepto, deleted) VALUES ($1, $2, $3)";
+    const values = [idUsuario, concepto, false];
+    const result = await pool.query(query, values);
+
     if (result) {
-      res.send("creating a services");
+      res.status(200).json({ message: "creating a services" });
     }
   } catch (error) {
     console.log(error);
   }
 };
 
-module.exports = { getAllServices, createServices };
+const updateServices = async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const query = "UPDATE services SET deleted = $1 WHERE idServicio = $2";
+    const values = [true, id];
+
+    const result = await pool.query(query, values);
+    if (result) {
+      res.status(200).json({ message: "creating a services" });
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+module.exports = { getAllServices, createServices, updateServices };
--- src/routes/services.routes.js
+++ src/routes/services.routes.js
@@ -2,10 +2,12 @@ const { Router } = require("express");
 const {
   getAllServices,
   createServices,
+  updateServices,
 } = require("../controllers/services.controllers");
 const router = Router();
 
 router.get("/services", getAllServices);
 router.post("/services", createServices);
+router.put("/services/:id", updateServices);
 
 module.exports = router;
