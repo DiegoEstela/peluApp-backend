54d7b1f1ac68fc971c2bca84782a233e534ef7bb
DiegoEstela <die.estela@gmail.com>
2023-06-05 18:19:32 +0200

    update expenses

--- src/controllers/expenses.controllers.js
+++ src/controllers/expenses.controllers.js
@@ -12,14 +12,14 @@ const getAllExpenses = async (req, res) => {
 
 const createExpenses = async (req, res, next) => {
   try {
-    const { servicio_id, valor } = req.body;
+    const { idUsuario, idServicio, monto, metodoPago } = req.body;
     const fecha = moment().format("YYYY-MM-DD");
     const query =
-      "INSERT INTO expenses (fecha, servicio_id, valor) VALUES ($1, $2, $3) RETURNING *";
-    const values = [fecha, servicio_id, valor];
+      "INSERT INTO expenses (fecha,idUsuario, idServicio, monto ,metodoPago) VALUES ($1, $2, $3, $4, $5) RETURNING *";
+    const values = [fecha, idUsuario, idServicio, monto, metodoPago];
     const result = await pool.query(query, values);
     if (result) {
-      res.send("creating a new expense");
+      res.status(200).json({ message: "creating a new expense" });
     }
   } catch (error) {
     console.log(error);
